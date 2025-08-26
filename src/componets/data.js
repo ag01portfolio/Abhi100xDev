984db4ee8f1432b2fee4559e34d9e706a5dc3413
Abhi - ROG <ag01portfolio@gmail.com>
2025-08-27 05:20:07 +0530

    Adding dummy data just for testing

---
+++ src/componets/data.js
@@ -0,0 +1,53 @@
+export const dataSet = {
+  Apps: [
+    {
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+      title: "React",
+      text: "A JavaScript library for building user interfaces."
+    },
+    {
+      imageUrl: "https://vitejs.dev/logo.svg",
+      title: "Vite",
+      text: "Next Generation Frontend Tooling."
+    },
+    {
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+      title: "HTML5",
+      text: "Standard markup language for documents designed to be displayed in a web browser."
+    },
+    {
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+      title: "CSS3",
+      text: "Style sheet language used for describing the presentation of a document written in HTML."
+    },
+    {
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+      title: "JavaScript",
+      text: "Programming language that conforms to the ECMAScript specification."
+    }
+  ],
+  Games: [
+    {
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/1055/1055687.png",
+      title: "Chess",
+      text: "Classic strategy board game."
+    },
+    {
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/1055/1055672.png",
+      title: "Sudoku",
+      text: "Logic-based, combinatorial number-placement puzzle."
+    }
+  ],
+  Achievement: [
+    {
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      title: "React Developer",
+      text: "Certified React Developer by XYZ Institute."
+    },
+    {
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      title: "Frontend Mastery",
+      text: "Frontend Development Certificate by ABC Academy."
+    }
+  ]
+};
