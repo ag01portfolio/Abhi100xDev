1a97b43f4e97cedc7ac53263aeb78fd71aa6419a
Abhi - ROG <ag01portfolio@gmail.com>
2025-08-28 23:54:50 +0530

    data

--- src/componets/data.js
+++ src/componets/data.js
@@ -27,6 +27,26 @@ export const dataSet = {
     }
   ],
   Games: [
+        {
+      imageUrl: "https://vitejs.dev/logo.svg",
+      title: "Vite",
+      text: "Next Generation Frontend Tooling."
+    },
+    {
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+      title: "HTML5",
+      text: "Standard markup language for documents designed to be displayed in a web browser."
+    },
+    {
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+      title: "CSS3",
+      text: "Style sheet language used for describing the presentation of a document written in HTML."
+    },
+    {
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+      title: "JavaScript",
+      text: "Programming language that conforms to the ECMAScript specification."
+    },
     {
       imageUrl: "https://cdn-icons-png.flaticon.com/512/1055/1055687.png",
       title: "Chess",
@@ -40,14 +60,24 @@ export const dataSet = {
   ],
   Achievement: [
     {
-      imageUrl: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
-      title: "React Developer",
-      text: "Certified React Developer by XYZ Institute."
+      imageUrl: "https://vitejs.dev/logo.svg",
+      title: "Vite",
+      text: "Next Generation Frontend Tooling."
     },
     {
-      imageUrl: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
-      title: "Frontend Mastery",
-      text: "Frontend Development Certificate by ABC Academy."
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+      title: "HTML5",
+      text: "Standard markup language for documents designed to be displayed in a web browser."
+    },
+    {
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+      title: "CSS3",
+      text: "Style sheet language used for describing the presentation of a document written in HTML."
+    },
+    {
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+      title: "JavaScript",
+      text: "Programming language that conforms to the ECMAScript specification."
     }
   ]
 };
